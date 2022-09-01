6ad0aeb707a5519bc1ab73586ff4b36d9eaeff32
Vahid <Vahid@flipsidecrypto.com>
2022-09-01 17:59:46 -0400

    new tables

---
+++ models/sushi/sushi__dim_dex_pools.sql
@@ -0,0 +1,18 @@
+{{ config(
+    materialized = 'table'
+) }}
+
+    SELECT
+        lower(pool_address) as pool_address,
+        lower(token0_address) as token0_address,
+        pool_name,
+        token0_symbol,
+        lower(token1_address) as token1_address,
+        token1_symbol,
+        token0_decimals,
+        token1_decimals
+    FROM
+         {{ source(
+            'avalanche_pools',
+            'SUSHI_DIM_DEX_POOLS'
+        ) }} 
---
+++ models/sushi/sushi__dim_kashi_pairs.sql
@@ -0,0 +1,18 @@
+{{ config(
+    materialized = 'table'
+) }}
+
+    SELECT
+        lower(kashi_pair_address) as pair_address, 
+        pair_name,
+        asset_token_symbol as asset_symbol,
+        lower(asset_token_address) as asset_address,
+        collateral_token_symbol as collateral_symbol,
+        lower(collateral_token_address) as collateral_address,
+        asset_token_decimals as asset_decimals,
+        collateral_token_decimals as collateral_decimals  
+    FROM
+         {{ source(
+            'avalanche_pools',
+            'SUSHI_DIM_KASHI_PAIRS'
+        ) }} 
