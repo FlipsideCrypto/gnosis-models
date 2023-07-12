35d1771026aaf94cc80a40e4d6533f489e432530
mattromano <mattromano88@gmail.com>
2023-07-12 14:09:13 -0700

    create_udfs macro changes

---
+++ macros/create_udfs.sql
@@ -0,0 +1,10 @@
+{% macro create_udfs() %}	
+    {% if var("UPDATE_UDFS_AND_SPS") %}	
+        {% set sql %}	
+        CREATE schema if NOT EXISTS silver;	
+
+        {% endset %}	
+        {% do run_query(sql) %}	
+        {{- fsc_utils.create_udfs() -}}	
+    {% endif %}	
+{% endmacro %}
