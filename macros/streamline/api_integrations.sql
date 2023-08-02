fd213d2d4b5424570308b4bfb7ffba01b6593406
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-08-02 13:32:54 -0400

    Gnosis streamline

---
+++ macros/streamline/api_integrations.sql
@@ -0,0 +1,21 @@
+{% macro create_aws_gnosis_api() %}
+    {{ log(
+        "Creating integration for target:" ~ target
+    ) }}
+
+    {% if target.name == "prod" %}
+        {% set sql %}
+        CREATE api integration IF NOT EXISTS aws_gnosis_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/gnosis-api-prod-rolesnowflakeudfsAF733095-FFKP94OAGPXW' api_allowed_prefixes = (
+            'https://xxx.execute-api.us-east-1.amazonaws.com/prod/'
+        ) enabled = TRUE;
+{% endset %}
+        {% do run_query(sql) %}
+        {% elif target.name == "dev" %}
+        {% set sql %}
+        CREATE api integration IF NOT EXISTS aws_gnosis_api_dev api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/gnosis-api-dev-rolesnowflakeudfsAF733095-KZU75JVH7RGG' api_allowed_prefixes = (
+            'https://fp4z9mbqa3.execute-api.us-east-1.amazonaws.com/dev/'
+        ) enabled = TRUE;
+{% endset %}
+        {% do run_query(sql) %}
+    {% endif %}
+{% endmacro %}
