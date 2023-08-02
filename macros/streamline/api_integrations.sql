fa7fea4c02a7c4b55cfb0bcdfbd79714a85ed634
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-08-02 17:10:31 -0400

    Update xxx

--- macros/streamline/api_integrations.sql
+++ macros/streamline/api_integrations.sql
@@ -5,8 +5,8 @@
 
     {% if target.name == "prod" %}
         {% set sql %}
-        CREATE api integration IF NOT EXISTS aws_gnosis_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/gnosis-api-prod-rolesnowflakeudfsAF733095-FFKP94OAGPXW' api_allowed_prefixes = (
-            'https://xxx.execute-api.us-east-1.amazonaws.com/prod/'
+        CREATE api integration IF NOT EXISTS aws_gnosis_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/gnosis-api-prod-rolesnowflakeudfsAF733095-1S5YHE2BPGRAE' api_allowed_prefixes = (
+            'https://abjc1ljs1d.execute-api.us-east-1.amazonaws.com/prod/'
         ) enabled = TRUE;
 {% endset %}
         {% do run_query(sql) %}
