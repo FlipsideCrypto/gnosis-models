a650a7607dca8e19e5ec1b33002d41dd0f7997d1
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-08-03 15:04:06 -0400

    Bump up loads again

--- models/silver/streamline/core/realtime/streamline__qn_getBlockWithReceipts_realtime.sql
+++ models/silver/streamline/core/realtime/streamline__qn_getBlockWithReceipts_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_bulk_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'qn_getBlockWithReceipts', 'sql_limit', {{var('sql_limit','50000')}}, 'producer_batch_size', {{var('producer_batch_size','50000')}}, 'worker_batch_size', {{var('worker_batch_size','12500')}}, 'batch_call_limit', {{var('batch_call_limit','10')}}, 'call_type', 'batch'))",
+        func = "{{this.schema}}.udf_bulk_json_rpc(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'qn_getBlockWithReceipts', 'sql_limit', {{var('sql_limit','150000')}}, 'producer_batch_size', {{var('producer_batch_size','75000')}}, 'worker_batch_size', {{var('worker_batch_size','15000')}}, 'batch_call_limit', {{var('batch_call_limit','10')}}, 'call_type', 'batch'))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
