5e6aa3f46b42481282391bac746c07f81c9af622
drethereum <trevor.wenokur@gmail.com>
2023-08-23 16:40:44 -0600

    transfers tables

--- models/gold/core__ez_token_transfers.sql
+++ models/gold/core__ez_token_transfers.sql
@@ -11,26 +11,28 @@ SELECT
     origin_function_signature,
     origin_from_address,
     origin_to_address,
-    contract_address,
+    t.contract_address,
     from_address,
     to_address,
     raw_amount,
-    decimals,
-    symbol,
+    raw_amount_precise,
+    C.token_decimals AS decimals,
+    C.token_symbol AS symbol,
     price AS token_price,
     CASE
-        WHEN decimals IS NOT NULL THEN raw_amount / pow(
+        WHEN C.token_decimals IS NOT NULL THEN raw_amount / pow(
             10,
-            decimals
+            C.token_decimals
         )
         ELSE NULL
     END AS amount,
     CASE
-        WHEN decimals IS NOT NULL AND price IS NOT NULL THEN amount * price
+        WHEN C.token_decimals IS NOT NULL
+        AND price IS NOT NULL THEN amount * price
         ELSE NULL
     END AS amount_usd,
     CASE
-        WHEN decimals IS NULL THEN 'false'
+        WHEN C.token_decimals IS NULL THEN 'false'
         ELSE 'true'
     END AS has_decimal,
     CASE
@@ -39,7 +41,14 @@ SELECT
     END AS has_price,
     _log_id
 FROM
-    {{ ref('core__fact_token_transfers') }} t
-LEFT JOIN {{ ref('core__fact_hourly_token_prices') }} p 
-    ON t.contract_address = p.token_address 
-        AND DATE_TRUNC('hour', t.block_timestamp) = HOUR
+    {{ ref('core__fact_token_transfers') }}
+    t
+    LEFT JOIN {{ ref('core__fact_hourly_token_prices') }}
+    p
+    ON t.contract_address = p.token_address
+    AND DATE_TRUNC(
+        'hour',
+        t.block_timestamp
+    ) = HOUR
+    LEFT JOIN {{ ref('silver__contracts') }} C
+    ON t.contract_address = C.contract_address
--- models/gold/core__ez_xdai_transfers.sql
+++ models/gold/core__ez_xdai_transfers.sql
@@ -1,37 +1,114 @@
 {{ config(
-    materialized = 'view',
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = 'block_number',
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['core','non_realtime'],
     persist_docs ={ "relation": true,
     "columns": true }
 ) }}
 
+WITH xdai_base AS (
+
+    SELECT
+        tx_hash,
+        block_number,
+        block_timestamp,
+        identifier,
+        from_address,
+        to_address,
+        xdai_value,
+        _call_id,
+        _inserted_timestamp,
+        to_varchar(
+            TO_NUMBER(REPLACE(DATA :value :: STRING, '0x'), REPEAT('X', LENGTH(REPLACE(DATA :value :: STRING, '0x'))))
+        ) AS xdai_value_precise_raw,
+        IFF(LENGTH(xdai_value_precise_raw) > 18, LEFT(xdai_value_precise_raw, LENGTH(xdai_value_precise_raw) - 18) || '.' || RIGHT(xdai_value_precise_raw, 18), '0.' || LPAD(xdai_value_precise_raw, 18, '0')) AS rough_conversion,
+        IFF(
+            POSITION(
+                '.000000000000000000' IN rough_conversion
+            ) > 0,
+            LEFT(rough_conversion, LENGTH(rough_conversion) - 19),
+            REGEXP_REPLACE(
+                rough_conversion,
+                '0*$',
+                ''
+            )
+        ) AS xdai_value_precise,
+        tx_position,
+        trace_index
+    FROM
+        {{ ref('silver__traces') }}
+    WHERE
+        xdai_value > 0
+        AND tx_status = 'SUCCESS'
+        AND trace_status = 'SUCCESS'
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '72 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+),
+tx_table AS (
+    SELECT
+        block_number,
+        tx_hash,
+        from_address AS origin_from_address,
+        to_address AS origin_to_address,
+        origin_function_signature
+    FROM
+        {{ ref('silver__transactions') }}
+    WHERE
+        tx_hash IN (
+            SELECT
+                DISTINCT tx_hash
+            FROM
+                xdai_base
+        )
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        MAX(_inserted_timestamp) - INTERVAL '72 hours'
+    FROM
+        {{ this }}
+)
+{% endif %}
+)
 SELECT
-    A.tx_hash AS tx_hash,
-    A.block_number AS block_number,
-    A.block_timestamp AS block_timestamp,
-    A.identifier AS identifier,
-    tx.from_address AS origin_from_address,
-    tx.to_address AS origin_to_address,
+    tx_hash AS tx_hash,
+    block_number AS block_number,
+    block_timestamp AS block_timestamp,
+    identifier AS identifier,
+    origin_from_address,
+    origin_to_address,
     origin_function_signature,
-    A.from_address AS xdai_from_address,
-    A.to_address AS xdai_to_address,
-    A.xdai_value AS amount,
+    from_address AS xdai_from_address,
+    to_address AS xdai_to_address,
+    xdai_value AS amount,
+    xdai_value_precise_raw AS amount_precise_raw,
+    xdai_value_precise AS amount_precise,
     ROUND(
-        A.xdai_value * price,
+        xdai_value * price,
         2
     ) AS amount_usd,
-    _call_id
+    _call_id,
+    _inserted_timestamp,
+    tx_position,
+    trace_index
 FROM
-    {{ ref('silver__traces') }} A
+    xdai_base A
     LEFT JOIN {{ ref('silver__prices') }}
     ON DATE_TRUNC(
         'hour',
-        block_timestamp
+        A.block_timestamp
     ) = HOUR
     AND token_address = LOWER('0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d')
-    LEFT JOIN {{ ref('silver__transactions') }}
-    tx
-    ON A.tx_hash = tx.tx_hash
-WHERE
-    A.xdai_value > 0
-    AND A.tx_status = 'SUCCESS'
-    AND A.trace_status = 'SUCCESS'
+    JOIN tx_table USING (
+        tx_hash,
+        block_number
+    )
