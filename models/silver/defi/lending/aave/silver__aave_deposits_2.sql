d4b8904a437bfaf2174f4e837b463680f0d6d09c
mattromano <mattromano88@gmail.com>
2024-05-10 16:05:12 -0700

    deposit_macro

---
+++ models/silver/defi/lending/aave/silver__aave_deposits_2.sql
@@ -0,0 +1,9 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = "block_number",
+    cluster_by = ['block_timestamp::DATE'],
+    tags = ['reorg','curated']
+) }}
+
+{{a_deposits ('silver.aave_tokens')}}
