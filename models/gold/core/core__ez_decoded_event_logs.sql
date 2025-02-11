a52c1da6c63927b1f0859bdc1b76980c5b35ade6
sam <sam@flipsidecrypto.com>
2025-02-11 21:57:43 +0800

    add table

--- models/gold/core/core__ez_decoded_event_logs.sql
+++ models/gold/core/core__ez_decoded_event_logs.sql
@@ -1,25 +1,28 @@
-{{ config(
-    materialized = 'view',
-    persist_docs ={ "relation": true,
-    "columns": true }
+{{ config (
+    materialized = "incremental",
+    unique_key = "ez_decoded_event_logs_id",
+    incremental_strategy = 'delete+insert',
+    cluster_by = "block_timestamp::date",
+    post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(ez_decoded_event_logs_id, contract_name, contract_address)",
+    tags = ['decoded_logs']
 ) }}
 
 SELECT
     block_number,
     block_timestamp,
     tx_hash,
-    {#tx_position, #} -- new column
+    {# tx_position,  #} -- new column
     event_index,
     contract_address,
     topics,
     topics [0] :: STRING AS topic_0,
-    -- new column
+    --new column
     topics [1] :: STRING AS topic_1,
-    -- new column
+    --new column
     topics [2] :: STRING AS topic_2,
-    -- new column
+    --new column
     topics [3] :: STRING AS topic_3,
-    -- new column
+    --new column
     DATA,
     event_removed,
     origin_from_address,
@@ -41,8 +44,18 @@ SELECT
     ) AS ez_decoded_event_logs_id,
     GREATEST(COALESCE(l.inserted_timestamp, '2000-01-01'), COALESCE(C.inserted_timestamp, '2000-01-01')) AS inserted_timestamp,
     GREATEST(COALESCE(l.modified_timestamp, '2000-01-01'), COALESCE(C.modified_timestamp, '2000-01-01')) AS modified_timestamp,
-    tx_status -- deprecate
+    tx_status --deprecate
 FROM
     {{ ref('silver__decoded_logs') }}
     l
     LEFT JOIN {{ ref('silver__contracts') }} C USING (contract_address)
+WHERE
+    1 = 1
+
+{% if is_incremental() %}
+AND l.modified_timestamp > (
+    SELECT
+        COALESCE(MAX(modified_timestamp), '2000-01-01' :: TIMESTAMP)
+    FROM
+        {{ this }})
+    {% endif %}
